110b3bc4a3ba051909f61647c24e175409b82478
Jair124 <botelloalan332@gmail.com>
2023-02-08 11:00:07 -0600

    terminamos todo

--- routes/auth.js
+++ routes/auth.js
@@ -16,6 +16,14 @@ const schemaLogin = Joi.object({
     password: Joi.string().min(6).required()
 })
 
+const schemaUpdate = Joi.object({
+    id: Joi.string().max(1024).required(),
+    name: Joi.string().min(6).max(255).required(),
+    lastname: Joi.string().max(255).required(),
+    email: Joi.string().max(1024).required(),
+    password: Joi.string().min(6).required()
+})
+
 router.post('/register', async(req, res) => {
 
     //Validacion de usuario
@@ -122,4 +130,49 @@ router.post('/borrar', async(req, res) => {
         })
     }
 })
+
+router.post('/update', async (req, res) => {
+        //Validacion de usuario
+        const{ error } = schemaUpdate.validate(req.body)
+        if (error) {
+            return res.status(400).json({
+                message : 'error al guardar',
+                error: error.details[0].message
+            })
+        }
+    
+        const isEmailUnique = await User.findOne({email: req.body.email})
+        if (isEmailUnique){
+            return res.status(400).json({
+                error: "Ese correo ya existe, no podemos actializar el usuario"
+            })
+        } 
+    
+        const salt = await bcrypt.genSalt(10)
+        const passwordEncriptado = await bcrypt.hash(req.body.password, salt)
+    
+        const usuario = ({
+            name: req.body.name,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: passwordEncriptado
+        })
+    
+        try {
+            const actualizado = await User.findByIdAndUpdate(req.body.id, usuario, {nex: true})
+            res.json({
+                message: 'Success Update',
+                data: actualizado
+            })
+        } catch (error) {
+            res.status(400).json({
+                message : 'error al actualizar',
+                error
+            })
+        }
+})
+
+
+
+
 module.exports = router
